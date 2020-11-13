aff634c26136bd959fc64bf3a566febfff63cd3f
Paweł Kopiczko <pawel@giantswarm.io>
2020-11-13 17:07:09 +0000

    Check again previous URL for opsctl

--- Formula/opsctl.rb
+++ Formula/opsctl.rb
@@ -6,7 +6,7 @@ require_relative "../custom_download_strategy.rb"
 class Opsctl < Formula
   desc "Controls things on Giant Swarm"
   homepage "https://github.com/giantswarm/opsctl"
-  url "https://github.com/giantswarm/opsctl/releases/download/v0.2.1/opsctl-v0.2.1-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://api.github.com/repos/giantswarm/opsctl/releases/assets/28287454", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.2.1"
   # openssl dgst -sha256 <file>
   sha256 "d51877bfdb9fc5ab70bd263b148b95f1f314b8169366d3cd7521f537e304b59e"
